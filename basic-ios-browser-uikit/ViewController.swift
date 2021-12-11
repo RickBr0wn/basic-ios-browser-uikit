e0c4dcf71023bbffabdf6f713ef62e7c7ab717b5
Rick Brown <ricky.brown.00@gmail.com>
2021-12-11 12:54:05 +0000

    Initial Commit

---
+++ basic-ios-browser-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basic-ios-browser-uikit
+//
+//  Created by Rick Brown on 11/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
